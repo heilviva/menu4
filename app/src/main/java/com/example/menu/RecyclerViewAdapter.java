d33fcef6785e112343689be07ec416ac63fbbfbe
ep777 <ep777@MAIBENBEN-PC>
2024-11-30 02:14:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/menu/RecyclerViewAdapter.java
@@ -0,0 +1,69 @@
+package com.example.menu;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private final Context context;
+    private final ArrayList<Book> bookArrayList;
+    private OnItemClickListener onItemClickListener;
+
+    public RecyclerViewAdapter(Context context, ArrayList<Book> bookArrayList) {
+        this.context = context;
+        this.bookArrayList = bookArrayList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.book_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
+        Book book = bookArrayList.get(position);
+        holder.bookName.setText(book.getBook_Name());
+        holder.bookAuthor.setText(book.getBook_Author());
+
+        holder.itemView.setOnClickListener(v -> {
+            if (onItemClickListener != null) {
+                onItemClickListener.onItemClick(book);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView bookName;
+        TextView bookAuthor;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bookName = itemView.findViewById(R.id.b_name);
+            bookAuthor = itemView.findViewById(R.id.b_author);
+        }
+    }
+
+
+    public interface OnItemClickListener {
+        void onItemClick(Book book);
+    }
+
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.onItemClickListener = listener;
+    }
+}
